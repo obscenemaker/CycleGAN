191d2160a3aa9742de33af2340b7436e327f7820
/Users/bytedance <zk.zk@bytedance.com>
2025-05-02 13:34:07 +0800

    fix123

--- datasets/prepare_cityscapes_dataset.py
+++ datasets/prepare_cityscapes_dataset.py
@@ -17,7 +17,9 @@ python prepare_cityscapes_dataset.py --gitFine_dir ./gtFine/ --leftImg8bit_dir .
 def load_resized_img(path):
     print(123)
     print(123123323123)
-    print(e213123)
+    print(213123)
+    print(123123)
+    print(3123123)
     return Image.open(path).convert('RGB').resize((256, 256))
 
 def check_matching_pair(segmap_path, photo_path):
