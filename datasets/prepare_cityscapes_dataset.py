014f65e7293a098fa67c704b87ae7ce52034dad2
rubik <90312062+obscenemaker@users.noreply.github.com>
2025-05-02 13:04:45 +0800

    Update prepare_cityscapes_dataset.py
    fix1231

--- datasets/prepare_cityscapes_dataset.py
+++ datasets/prepare_cityscapes_dataset.py
@@ -17,6 +17,7 @@ python prepare_cityscapes_dataset.py --gitFine_dir ./gtFine/ --leftImg8bit_dir .
 def load_resized_img(path):
     print(123)
     print(321)
+    print(123123123)
     return Image.open(path).convert('RGB').resize((256, 256))
 
 def check_matching_pair(segmap_path, photo_path):
