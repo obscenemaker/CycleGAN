d55607f99ae7d229ae177378b5c2b7786528701b
/Users/bytedance <zk.zk@bytedance.com>
2025-05-02 13:07:33 +0800

    fix600

--- datasets/prepare_cityscapes_dataset.py
+++ datasets/prepare_cityscapes_dataset.py
@@ -18,7 +18,7 @@ def load_resized_img(path):
     print(123)
     print(321)
     print(123123123)
-    print(23123123123)
+    print(312312321)
     return Image.open(path).convert('RGB').resize((256, 256))
 
 def check_matching_pair(segmap_path, photo_path):
